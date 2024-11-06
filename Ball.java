f29a250bdeb280bc47eaae244def7e00c58019c2
Jasmine Tann <153488776+jstann@users.noreply.github.com>
2024-11-06 10:30:01 -0500

    Create Ball.java

---
+++ Ball.java
@@ -0,0 +1,19 @@
+import javafx.scene.shape.Circle;
+
+public class Ball extends Circle {
+  private int speed;
+  private dx;
+  private dy;
+
+public Ball(int xloc, int yloc) {
+  this.setCenterX(xLoc);
+  this.setCenterY(yLoc);
+  this.setStroke(Color.RED);
+  this.setFill(Color.RED);
+  dx = 0;
+  dy = 1;
+
+}
+  public void move() {
+    this.setCenterX( this.getCenterX() + dx*speed);
+    this.setCenterY( this.getCenterY() + dy*spedd);
